4758fa0d4b795096fbbcc47c183ced5c29878945
Sivan Wolberg <sivan@wolberg.pro>
2021-07-07 00:21:51 +0300

    rework test confgi and file pattes

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,9 +1,20 @@
+import { chromium, Browser, Page } from 'playwright'
 // Needs to be higher than the default Playwright timeout
 jest.setTimeout(40 * 1000)
 // this test it is general test see jest working with Playwright so basic test on github
 describe('GitHub', () => {
-    it('should show the microsoft/Playwright project in the search if you search for Playwright', async () => {
+    let browser: Browser
+    let page: Page
+    beforeAll(async () => {
+        browser = await chromium.launch()
+        page = await browser.newPage()
         await page.goto('https://github.com')
+    })
+
+    afterAll(async () => {
+        await browser.close()
+    })
+    it('should show the microsoft/Playwright project in the search if you search for Playwright', async () => {
         await page.type('input[name="q"]', 'Playwright')
         await page.press('input[name="q"]', 'Enter')
         await expect(page).toHaveText('.repo-list', 'microsoft/playwright')
--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,29 @@
 import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 
 const config: InitialOptionsTsJest = {
+    roots: ['<rootDir>'],
     verbose: true,
     preset: 'jest-playwright-preset',
-    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+    testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
+    setupFilesAfterEnv: ['expect-playwright', '<rootDir>/setupTests.ts'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
+    },
+    testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+    testEnvironmentOptions: {
+        'jest-playwright': {
+            browsers: ['chromium', 'firefox', 'webkit'],
+            exitOnPageError: false, // GitHub currently throws errors
+            launchOptions: {
+                headless: true,
+            },
+        },
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.test.json',
+        },
     },
 }
 export default config
