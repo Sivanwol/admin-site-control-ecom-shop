a54bf9d1774cd34f456dd28003fcfc93ca079f05
Sivan Wolberg <sivan@wolberg.pro>
2021-07-06 23:55:29 +0300

    bit update on the todos, small changes on the just config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 const config: InitialOptionsTsJest = {
     verbose: true,
     preset: 'jest-playwright-preset',
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
     },
